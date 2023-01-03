6b391c74b87e79e06264658df4f80278ab454caf
ryelsie <121794339+ryelsie@users.noreply.github.com>
2023-01-03 12:37:07 +0700

    Add files via upload

---
+++ Practicing Input data.py
@@ -0,0 +1,16 @@
+#data yang dimasukkan pasti string
+data = input("Masukan data: ")
+
+print("data = ",data,",type =",type(data))
+
+#jika kita ingin mengambil int, maka 
+angka = float(input("Masukan angka: "))
+angka = int(input("Masukan angka: "))
+
+print("data = ",angka,",type =",type(angka))
+
+#bagaimana dengan boolean
+
+biner = bool(int(input("Masukan nilai boolean: ")))
+
+print("data = ",biner,",type =",type(biner))
