e9c4a9ba9418f2fb673f10ea2ea537b301e99d28
ryelsie <121794339+ryelsie@users.noreply.github.com>
2023-01-03 12:36:46 +0700

    Add files via upload

---
+++ Practicing Converting Celcius into other.py
@@ -0,0 +1,14 @@
+#Program Konversi Celcius Ke satuan Lain
+
+kelvin = float(input('Masukan suhu dalam kelvin : '))
+
+print("suhu adalah",kelvin, "Kelvin")
+# Kelvin ke Fahrenheit dan sebaliknya
+fahrenheit = (9/5) * ((kelvin - 273)) + 32
+print("suhu dalam fahrenheit adalah ",fahrenheit, "")
+
+
+
+
+
+
